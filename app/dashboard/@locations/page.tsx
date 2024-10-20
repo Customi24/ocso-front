b3450e1bcb69b7d7f9c59fa50fd9d07ebb2acb7b
Customi24 <131020560+Customi24@users.noreply.github.com>
2024-10-19 21:41:08 -0500

    Location card

--- app/dashboard/@locations/page.tsx
+++ app/dashboard/@locations/page.tsx
@@ -3,10 +3,11 @@ import { cookies } from "next/headers";
 import { TOKEN_NAME } from "@/constants";
 import { Location } from "@/entities";
 import SelectLocation from "./_components/SelectLocation";
+import LocationCard from "./_components/LocationCard";
 
 
-const LocationsPage = async ({searchParams}: {
-    searchParams: { [key: string]: string | string[] | undefined};
+const LocationsPage = async ({ searchParams }: {
+    searchParams: { [key: string]: string | string[] | undefined };
 }) => {
     const userCookies = cookies();
     const token = userCookies.get(TOKEN_NAME)?.value
@@ -17,19 +18,22 @@ const LocationsPage = async ({searchParams}: {
     },);
     data = [
         {
-        locationId:0,
-        locationName: "Ninguna",
-        locationLatLng: [0,0],
-        locationAddress: "No existe"
+            locationId: 0,
+            locationName: "Ninguna",
+            locationLatLng: [0, 0],
+            locationAddress: "No existe"
         },
         ...data
     ]
 
     return (
-        <div className="w-8/12">
+        <div className="w-7/12">
             <div className="w-full flex flex-col itens-center h-[90vh] bg-red-50">
                 <div className="w-1/2 my-10">
                     <SelectLocation locations={data} store={searchParams?.store} />
+                </div>
+                <div className="w-8/12">
+                    <LocationCard store={searchParams.store} />
                 </div>
             </div>
         </div>
